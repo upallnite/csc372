0484f8d2fd718ed648ab34c5ee2763a58e8b8285
Alex Tough <MapleArepa@user3-217-169.wireless.utoronto.ca>
2013-09-25 15:13:39 -0400

    Implement test cases except for WaitlistEnqueue

--- labs/1/test.c
+++ labs/1/test.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 typedef int ProcessId;
 
@@ -28,6 +29,7 @@ struct LL *CreateList(ListType type)
 	struct LL *newList;
 
 	if ((newList = malloc(sizeof(struct LL))) == NULL) {
+		printf("%s\n", "Error allocating space for a pointer to a list.");
 		return NULL;
 	}
 	newList->type = type;
@@ -39,11 +41,16 @@ struct LL *CreateList(ListType type)
 //dequeues the PD at the head of list and returns a pointer to it, or else null.
 struct PD * DequeueHead( struct LL *list )
 {
+
+	if(!list){
+		return NULL;
+	}
+
     struct PD *head;
 
     head = list->head;
 
-	if (head != NULL) {
+	if (head) {
     	list->head = (list->head)->link;
 	}
     return head;
@@ -53,76 +60,80 @@ struct PD * DequeueHead( struct LL *list )
 // successful, and -1 otherwise.
 RC DestroyList( struct LL *list ) 
 {
+	if(!list){
+		return FAIL;
+	}
 	if(list->head){
 		struct PD* current = list->head;
-		while(current != NULL){
+		while(current){
 			struct PD* old_head = DequeueHead(list);
 			free(old_head);
 			current = list->head;
 		}
 	}
-	if (list->head == NULL) {
+	if (!list->head) {
+		free(list);
 		return SUCC;
 	} 
-	return FAIL;
-/*
+
 	free(list);
-	list = NULL;
-	if (list == NULL) {
-		return SUCC;
-	}
 	return FAIL;
-	*/
+
 }
 
 //if list is a priority list, then enqueues pd in its proper location. 
 //Returns -1 if list is not a priority list and 0 otherwise.
 RC PriorityEnqueue(struct PD *pd, struct LL *list)
 {
-    struct PD *cur, *prev;
-	
-	if (list->type == L_PRIORITY) {    
-		if (list->head == NULL) {
-			list->head = pd;
-		} else if (pd->priority < (list->head)->priority){
-			pd->link = list->head;
-			list->head = pd;
-		} else {
-			prev = list->head;
-			cur = prev->link;
+	struct PD *cur, *prev;
 
-			while ((cur != NULL) && (pd->priority > cur->priority))
-			{
-				printf("hi");
-				prev = cur;
-  		  		cur = cur->link;
+    if(!pd || !list || list->type != L_PRIORITY){
+    	return FAIL;
+    }
+
+    pd->inlist = list;
+
+    if(!list->head){
+    	list->head = pd;
+    } else{
+    	prev = NULL;
+    	cur = list->head;
+    	while(cur){
+    		if(cur->priority > pd->priority){
+    			if(!prev){
+    				pd->link = list->head;
+    				list->head = pd;
+    			} else{
+    				pd->link = cur;
+    				prev->link = pd;
+    			}
+    			break;
     		}
-    		if (cur != NULL) { 
-				pd->link = prev->link;
-				prev->link = pd;			
-			} else {
-        		prev->link = pd;
-			}
-		}
-		pd->inlist = list;
-		return SUCC;
-	} else {
-		return FAIL;
-	}
+    		prev = cur;
+    		cur = cur->link;
+    	}
+    }
+    return SUCC;
 }
+
 
 //enqueues pd at the head of list if list is a LIFO list. Returns 0 if 
 //OK and -1 otherwise.
 RC EnqueueAtHead(struct PD *pd, struct LL *list)
 {
-    if (list->type == L_LIFO) {
-        pd->link = list->head;
-        list->head = pd;
-		pd->inlist = list;
-        return SUCC;
-    } else {
-        return FAIL;
+
+	struct PD *cur, *prev;
+
+    if(!pd || !list || list->type != L_LIFO){
+    	return FAIL;
     }
+
+    pd->link = list->head;
+    list->head = pd;
+    pd->inlist = list;
+
+    return SUCC;
+
 }
 
 void waitDiff(struct PD *pd, int diff) {
@@ -134,7 +145,11 @@ void waitDiff(struct PD *pd, int diff) {
 	}
 }
 
-RC WaitlistEnqueue( struct PD *pd, int waittime, struct LL *list)
+// If list is a waiting list, then inserts pd in its correct 
+// position assuming it should wait for waittime. The waittime 
+// values of the other elements in the list should be properly 
+// adjusted. Return -1 if list is not a waiting list and 0 otherwise.
+RC WaitlistEnqueue(struct PD *pd, int waittime, struct LL *list)
 {	
     struct PD *cur, *prev;
 
@@ -173,66 +188,56 @@ RC WaitlistEnqueue( struct PD *pd, int waittime, struct LL *list)
 	}
 }
 
+// The PD before the PD with process id pid in list, 
+// which has at least two nodes.
 struct PD * FindPrevPD(ProcessId pid, struct LL* list){
 	struct PD* prev = NULL;
 	struct PD* cur = list->head;
 	
-	while(cur != NULL){
-		//printf("pid: %d\n", cur->pid);
+	while(cur){
 		if(cur->pid == pid){
 			return prev;
 		}
 		prev = cur;
 		cur = cur->link;
-		//printf("p pid: %d\n", prev->pid);
 	}
 	return prev;
 }
 
+
+// Searches list for a PD with a process id pid, and 
+// returns a pointer to it or null otherwise.
 struct PD * FindPD(ProcessId pid, struct LL *list){
+	
+	if(!list || !list->head){
+		return NULL;
+	}
+
 	struct PD *prev;
 
-	if (list->head != NULL) { 
-		if (list->head->pid == pid) {
-			return list->head;
-		} else {
-			prev = FindPrevPD(pid, list);
-			return prev->link;
-		}
+	if (list->head->pid == pid) {
+		return list->head;
+	} else {
+		prev = FindPrevPD(pid, list);
+		return prev->link;
 	}
 
-/*	struct PD* current = list->head;
-	while(current != NULL){
+	return NULL;
 
-		if(current->pid == pid){
-			return current;
-		}
-
-		current = current->link;
-	}
-*/	return NULL;
 }
 
+// Dequeues pd from whatever list it might be in, 
+// if it is in one.
 void DequeuePD(struct PD* pd){
-/*
-	// Case 0: pd->inlist refers to an empty list.
-	if(!pd->inlist){;}
-	
-	//Case 1: pd->inlist refers to a singleton list.
-	if(!pd->inlist->head->link){
-		pd->inlist->head = NULL;
+
+	if(!pd){
+		return;
 	}
-					
-	// Case 2: pd->head refers to a list with two or more elements.
-	struct PD* prev = FindPrevPd(pd->pid, pd->inlist);
-	struct PD* next = prev->link->link;
-	free(prev->link);
-	prev->link = next;	
-*/
-	if (pd->inlist != NULL) { // Case 0 
-		if(pd->inlist->head == pd){ // Case 1
+
+	if (pd->inlist) { // inlist is not empty 
+		if(pd->inlist->head == pd){ // inlist only has one element
 			pd->inlist->head = pd->link;
-		} else { // Case 2
+		} else { // inlist has at least two elements
 			struct PD *prev, *next; 
 		
 			prev = FindPrevPD(pd->pid, pd->inlist);
@@ -245,59 +250,108 @@ void DequeuePD(struct PD* pd){
 
 typedef enum {False = 0, True = 1} bool;
 
+// Creates a new PD.
+struct PD* createPD(ProcessId pid, int priority, int waittime){
+	struct PD *pd = malloc(sizeof(struct PD));
+	if(!pd){
+		printf("Error allocating space for a pd pointer");
+		return NULL;
+	}
+	pd->pid = pid; pd->priority = priority; pd->waittime = waittime;
+	return pd;
+}
+
+void test(){
+
+	// Some PD structures to test on
+	struct PD *pd1 = createPD(1,4,0);
+	struct PD *pd2 = createPD(2,5,0);
+	struct PD *pd3 = createPD(3,3,0);
+	struct PD *pd4 = createPD(4,1,0);
+	struct PD *pd5 = createPD(5,2,0);
+
+
+	// Now we test for CreateList
+	struct LL *priority_list = CreateList(L_PRIORITY);
+	struct LL *wait_list = CreateList(L_WAITING);
+	struct LL *lifo_list = CreateList(L_LIFO);
+	struct LL *undef_list = CreateList(UNDEF);
+
+	assert(priority_list != NULL);
+	assert(wait_list != NULL);
+	assert(lifo_list != NULL);
+	assert(undef_list != NULL);
+
+	// PriorityEnqueue test
+	// Extreme cases: NULL values, 2^32 PDs (time permitting)
+	assert(PriorityEnqueue(NULL,NULL) == FAIL);
+	// General case
+	assert(PriorityEnqueue(pd1, priority_list) == SUCC);
+	assert(PriorityEnqueue(pd2, priority_list) == SUCC);
+	assert(PriorityEnqueue(pd3, priority_list) == SUCC);
+	assert(PriorityEnqueue(pd4, priority_list) == SUCC);
+	assert(PriorityEnqueue(pd5, priority_list) == SUCC);
+	assert(PriorityEnqueue(pd1, wait_list) == FAIL);
+	assert(PriorityEnqueue(pd1, lifo_list) == FAIL);
+	assert(PriorityEnqueue(pd1, undef_list) == FAIL);
+
+	// DequeueHead test
+	// Extreme cases: NULL values, 2^32 PDs (time permitting)
+	assert(DequeueHead(NULL) == NULL);
+	// General case
+	assert(DequeueHead(priority_list) == pd4);
+	assert(DequeueHead(wait_list) == NULL);
+	assert(DequeueHead(lifo_list) == NULL);
+	assert(DequeueHead(undef_list) == NULL);
+
+	// Check DestroyList, then EnqueueAtHead then WaitListEnqueue.
+	assert(DestroyList(NULL) == FAIL);
+	assert(DestroyList(priority_list) == SUCC);
+	assert(DestroyList(undef_list) == SUCC);
+
+	// NOTE: Priority list and undef list no longer exist!
+
+	// EnqueueAtHead test
+	pd1 = createPD(1,4,0);
+	pd2 = createPD(2,5,0);
+	pd3 = createPD(3,3,0);
+	pd4 = createPD(4,1,0);
+	pd5 = createPD(5,2,0);
+	assert(EnqueueAtHead(pd1, wait_list) == FAIL);
+	assert(EnqueueAtHead(pd1, lifo_list) == SUCC);
+	assert(EnqueueAtHead(pd2, lifo_list) == SUCC);
+	assert(EnqueueAtHead(pd3, lifo_list) == SUCC);
+	assert(EnqueueAtHead(pd4, lifo_list) == SUCC);
+	assert(EnqueueAtHead(pd5, lifo_list) == SUCC);
+
+	assert(DequeueHead(lifo_list) == pd5);
+	assert(DequeueHead(lifo_list) == pd4);
+
+	// NOTE: pd5 and pd4 are no longer in lifo_list!
+
+	// WaitlistEnqueue test
+
+	// FindPD test
+	assert(FindPD(1,NULL) == NULL);
+	assert(FindPD(4,lifo_list) == NULL);
+	assert(FindPD(1,lifo_list) == pd1);
+	assert(FindPD(2,lifo_list) == pd2);
+	assert(FindPD(3,lifo_list) == pd3);
+
+	// DequeuePD test
+	DequeuePD(NULL);
+	DequeuePD(pd1);
+	assert(FindPD(1,lifo_list) == NULL);
+	assert(FindPD(2,lifo_list) == pd2);
+
+	assert(DestroyList(wait_list) == SUCC);
+
+}
+
+
 int main(void) {
 
-	struct PD *pd1 = &(struct PD) {.pid = 1, .priority = 4};
-	struct PD *pd2 = &(struct PD) {.pid = 2, .priority = 5};
-	struct PD *pd3 = &(struct PD) {.pid = 3, .priority = 3};
-	struct PD *pd4 = &(struct PD) {.pid = 4, .priority = 1};
-	struct PD *pd5 = &(struct PD) {.pid = 5, .priority = 2};
-	
-	struct PD *hld1, *hld2, *hld3, *hld4, *hld5;
-	struct LL *list;
-	bool t1, t2, t3, t4, t5;
-
-	ListType type=L_PRIORITY;
-	list = CreateList(type);
-	
-	t1 = PriorityEnqueue(pd1,list);
-	t2 = PriorityEnqueue(pd2,list);
-	t3 = PriorityEnqueue(pd3,list);
-	t4 = PriorityEnqueue(pd4,list);
-	t5 = PriorityEnqueue(pd5,list);
-
-	hld1 = FindPrevPD(1, list);
-	hld2 = FindPrevPD(2, list);
-	hld3 = FindPrevPD(3, list);
-	hld4 = FindPrevPD(4, list);
-	hld5 = FindPrevPD(5, list);
-	
-	printf("pid: %d priority: %d\n", hld1->pid, hld1->priority);
-	printf("pid: %d priority: %d\n", hld2->pid, hld2->priority);
-	printf("pid: %d priority: %d\n", hld3->pid, hld3->priority);
-	//printf("pid: %d priority: %d\n", hld4->pid, hld4->priority); 
-	//causes SEGFAULT because FindPrevPD returns NULL
-	printf("pid: %d priority: %d\n", hld5->pid, hld5->priority);
-		
-	DequeuePD(pd1);
-	DequeuePD(pd2);
-	DequeuePD(pd3);
-	DequeuePD(pd4);
-	DequeuePD(pd5);
-
-	//dl = DestroyList(list);
-	
-	printf("t1: %d\nt2: %d\nt3: %d\nt4: %d\nt5: %d\n", t1, t2, t3, t4, t5);
-
-    /*
-	list = CreateList(L_WAITING);
-	t1 = WaitlistEnqueue( pd, 10, list);
-	pd2 = FindPD( 1, list ) ;
-	DequeuePD( pd2) ;
-	t3=DestroyList(list) ;
-	
-	printf("t1: %d\nt3: %d\n", t1, t3);*/
-	
+	test();
 	return 0;
 }
 
